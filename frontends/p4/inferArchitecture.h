57af7f123d729cc980d55bcda1d0d95687c2d7e2
Pierce McEntagart <pierce.mcentagart@barefootnetworks.com>
2016-12-19 16:40:24 -0800

    Add license -- need to address the necessity of a custom visit order for this pass later on.

--- frontends/p4/inferArchitecture.h
+++ frontends/p4/inferArchitecture.h
@@ -1,3 +1,19 @@
+/*
+Copyright 2013-present Barefoot Networks, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #ifndef _P4_INFER_ARCHITECTURE_H_
 #define _P4_INFER_ARCHITECTURE_H_
 
@@ -23,13 +39,11 @@ class InferArchitecture : public Inspector {
 
     ::P4_16::V2Model *getModel() { return this->archModel; }
 
-    // Control bock will need to "infer" if it is a deparser
     bool preorder(const IR::Type_Control *node) override;
     bool preorder(const IR::Type_Parser *node) override;
     bool preorder(const IR::Type_Extern *node) override;
     bool preorder(const IR::Type_Package *node) override;
 
-    // Needed just to start the traversal
     bool preorder(const IR::P4Program* program) override;
 
     // don't care
