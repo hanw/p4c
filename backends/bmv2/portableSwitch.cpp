487d5f167280d34a2516f4afe574c24d90ad5cda
Rakesh Sharma <rakeshsharma@Rakeshs-MacBook-Pro.local>
2018-05-04 18:24:53 -0400

    parser name in portableswitch.cpp

--- backends/bmv2/portableSwitch.cpp
+++ backends/bmv2/portableSwitch.cpp
@@ -361,13 +361,14 @@ bool InspectPsaProgram::preorder(const IR::Parameter* param) {
 
 bool InspectPsaProgram::preorder(const IR::P4Parser* parser) {
 
-    auto parser_id = json->add_parser("parser");
+    #auto parser_id = json->add_parser("parser");
 
     for (auto s : parser->parserLocals) {
         if (auto inst = s->to<IR::P4ValueSet>()) {
             auto bitwidth = inst->elementType->width_bits();
             auto name = inst->controlPlaneName();
-            json->add_parse_vset(name, bitwidth);
+            dump name
+            #json->add_parse_vset(name, bitwidth);
         }
     }
 
